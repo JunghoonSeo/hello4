1457e000c0a583ceb79517e777da9b9507a3e59b
junghoonseo <neom@naver.com>
2022-01-16 21:32:19 +0900

    second commit

--- hello4.c
+++ hello4.c
@@ -2,11 +2,16 @@
 
 main()
 {
-	int i = 0;
+	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i < 10; i++)
 	{
-		printf("Hello\n");
+		for (j = 1; j < i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+
 	}
 
 }
